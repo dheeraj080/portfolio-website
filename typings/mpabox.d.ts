3ef9c47f1a6cc667043dd0a8396a999a7ae9db91
dheeraj080 <kambledheerajkumar@gmail.com>
2026-03-03 14:15:06 +0530

    feat: Integrate Mapbox GL JS, geocoder, and add necessary type declarations for Mapbox modules.

---
+++ typings/mpabox.d.ts
@@ -0,0 +1,7 @@
+// This silences the "implicit type library" error for Mapbox internal modules
+declare module '@mapbox/point-geometry';
+declare module '@mapbox/vector-tile';
+declare module '@mapbox/unitbezier';
+declare module '@mapbox/whoots-js';
+declare module '@mapbox/geojson-vt';
+declare module '@mapbox/mapbox-gl-supported';
